440f687986254fc87771326593cab029c2cd9de5
colinbrush <97994812+colinbrush@users.noreply.github.com>
2022-02-23 20:26:59 -0700

    Mouse is big

--- DrawPlay/src/Mouse.java
+++ DrawPlay/src/Mouse.java
@@ -3,8 +3,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class Mouse {
-	private static final int BODY_LENGTH = 50;
-	private static final int BODY_HEIGHT = 20;
+	private static final int BODY_LENGTH = 200;
+	private static final int BODY_HEIGHT = 100;
 	private static final int TAIL_LENGTH = 40;
 	public void draw(Graphics g, int mouseX, int mouseY)
 	{
@@ -17,6 +17,6 @@ public class Mouse {
 		g2.drawLine(x, y, x2, y);
 		// text appears below mouse body, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("EEEK", mouseX, mouseY+BODY_HEIGHT+10);	
+		g2.drawString("SUP", mouseX, mouseY+BODY_HEIGHT+10);	
 	}
 }
